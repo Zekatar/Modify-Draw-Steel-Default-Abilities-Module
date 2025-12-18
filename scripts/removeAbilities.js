4f1ffbb4292f04d29529759683293c42147c9daf
Zekatar <stephan.linsert@gmx.de>
2025-12-18 19:56:44 +0100

    Add files via upload

--- scripts/removeAbilities.js
+++ scripts/removeAbilities.js
@@ -2,35 +2,23 @@ Hooks.once("ready", () => {
 //Find and save set of default abilities
 const set = CONFIG.DRAW_STEEL.hero.defaultItems;
 
-//Define abilities
-const advance = "Compendium.draw-steel.abilities.Item.ucR2C7lMvXrKIMZ7";
-const aidAttack = "Compendium.draw-steel.abilities.Item.Xb3S5N1fZyICD58D";
-const catchBreath = "Compendium.draw-steel.abilities.Item.nYPJN8Ce2dX9H09K";
-const charge = "Compendium.draw-steel.abilities.Item.wNqJWJbgAbnJBqZf";
-const defend = "Compendium.draw-steel.abilities.Item.fjtY7RKBGWx2u5tK";
-const disengage = "Compendium.draw-steel.abilities.Item.vBlTvHRZ5JBXWYt6";
-const escapeGrab = "Compendium.draw-steel.abilities.Item.iD1SlB15GXJFALya";
-const grab = "Compendium.draw-steel.abilities.Item.oxaISpgVoCfo6fmt";
-const heal = "Compendium.draw-steel.abilities.Item.2qWHDVB7SBS9anLB";
-const knockback = "Compendium.draw-steel.abilities.Item.emug9cXuwndDrWzu";
-const melee = "Compendium.draw-steel.abilities.Item.wU69Y06G9lYFrvp6";
-const ranged = "Compendium.draw-steel.abilities.Item.eqUobBcm81mqZVgJ";
-const ride = "Compendium.draw-steel.abilities.Item.QXOkflcYF6DITJE3";
-const standUp = "Compendium.draw-steel.abilities.Item.XeUU0Blvi0fy0b2G";
+//Find compendium with basic abilities
+var basicCompendium = game.packs.get("draw-steel.abilities");
 
-//Check settings and remove ability
-if (game.settings.get("modify-draw-steel-default-abilities", "addAdvance") === false) {set.delete(advance);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addAidAttack") === false) {set.delete(aidAttack);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addCatchBreath") === false) {set.delete(catchBreath);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addCharge") === false) {set.delete(charge);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addDefend") === false) {set.delete(defend);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addDisengage") === false) {set.delete(disengage);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addEscapeGrab") === false) {set.delete(escapeGrab);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addGrab") === false) {set.delete(grab);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addHeal") === false) {set.delete(heal);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addKnockback") === false) {set.delete(knockback);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addMelee") === false) {set.delete(melee);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addRanged") === false) {set.delete(ranged);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addRide") === false) {set.delete(ride);};
-if (game.settings.get("modify-draw-steel-default-abilities", "addStandUp") === false) {set.delete(standUp);};
+/* Find the ability folder by name, commented out because of possible translation issues
+//Find folder of basic abilities
+var basicFolder = basicCompendium.folders.find(a => a.name === "Basic Abilities");
+//Find basic abilities in folder and gather them in array
+var basicAbilities = basicCompendium.index.filter(a => a.folder === basicFolder._id);
+*/
+
+//Find basic abilities by folder id and gather them in array
+var basicAbilities = basicCompendium.index.filter(a => a.folder === "YKzQ5cCX8knW8fAc");
+
+
+//Check settings and remove ability from set in loop
+for (basicAbility of basicAbilities){
+  let settingID = "add" + basicAbility.name.replaceAll(" ", "");
+  if (game.settings.get("modify-draw-steel-default-abilities", settingID) === false) {set.delete(basicAbility.uuid);};
+};
 });
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,212 +1,37 @@
-Hooks.once("init", () => {
-game.settings.register("modify-draw-steel-default-abilities", "addAdvance", {
-  name: "Advance",
-  hint: "Advance is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const advance = "Compendium.draw-steel.abilities.Item.ucR2C7lMvXrKIMZ7";
-  if (value === true) {set.add(advance);}
-  else {set.delete(advance);};
-  }
- });
+Hooks.once("ready", () => {
+//Find compendium with basic abilities
+var basicCompendium = game.packs.get("draw-steel.abilities");
 
-game.settings.register("modify-draw-steel-default-abilities", "addAidAttack", {
-  name: "Aid Attack",
-  hint: "Aid Attack is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const aidAttack = "Compendium.draw-steel.abilities.Item.Xb3S5N1fZyICD58D";
-  if (value === true) {set.add(aidAttack);}
-  else {set.delete(aidAttack);};
-  }
- });
+/* Find the ability folder by name, commented out because of possible translation issues
+//Find folder of basic abilities
+var basicFolder = basicCompendium.folders.find(a => a.name === "Basic Abilities");
+//Find basic abilities in folder and gather them in array
+var basicAbilities = basicCompendium.index.filter(a => a.folder === basicFolder._id);
+*/
 
-game.settings.register("modify-draw-steel-default-abilities", "addCatchBreath", {
-  name: "Catch Breath",
-  hint: "Catch Breath is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const catchBreath = "Compendium.draw-steel.abilities.Item.nYPJN8Ce2dX9H09K";
-  if (value === true) {set.add(catchBreath);}
-  else {set.delete(catchBreath);};
-  }
- });
+//Find basic abilities by folder id and gather them in array
+var basicAbilities = basicCompendium.index.filter(a => a.folder === "YKzQ5cCX8knW8fAc");
 
-game.settings.register("modify-draw-steel-default-abilities", "addCharge", {
-  name: "Charge",
-  hint: "Charge is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const charge = "Compendium.draw-steel.abilities.Item.wNqJWJbgAbnJBqZf";
-  if (value === true) {set.add(charge);}
-  else {set.delete(charge);};
-  }
- });
+//Sort basic abilities by name
+basicAbilities.sort((a, b) => a.name.localeCompare(b.name));
 
-game.settings.register("modify-draw-steel-default-abilities", "addDefend", {
-  name: "Defend",
-  hint: "Defend is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const defend = "Compendium.draw-steel.abilities.Item.fjtY7RKBGWx2u5tK";
-  if (value === true) {set.add(defend);}
-  else {set.delete(defend);};
-  }
- });
+//Create setting entry for every basic ability
+for (let i = 0; i < basicAbilities.length; i++) {
+  let basicAbility = basicAbilities[i];
+  let settingID = "add" + basicAbility.name.replaceAll(" ", "");
 
-game.settings.register("modify-draw-steel-default-abilities", "addDisengage", {
-  name: "Disengage",
-  hint: "Disengage is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const disengage = "Compendium.draw-steel.abilities.Item.vBlTvHRZ5JBXWYt6";
-  if (value === true) {set.add(disengage);}
-  else {set.delete(disengage);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addEscapeGrab", {
-  name: "Escape Grab",
-  hint: "Escape Grab is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const escapeGrab = "Compendium.draw-steel.abilities.Item.iD1SlB15GXJFALya";
-  if (value === true) {set.add(escapeGrab);}
-  else {set.delete(escapeGrab);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addGrab", {
-  name: "Grab",
-  hint: "Grab is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const grab = "Compendium.draw-steel.abilities.Item.oxaISpgVoCfo6fmt";
-  if (value === true) {set.add(grab);}
-  else {set.delete(grab);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addHeal", {
-  name: "Heal",
-  hint: "Heal is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const heal = "Compendium.draw-steel.abilities.Item.2qWHDVB7SBS9anLB";
-  if (value === true) {set.add(heal);}
-  else {set.delete(heal);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addKnockback", {
-  name: "Knockback",
-  hint: "Knockback is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const knockback = "Compendium.draw-steel.abilities.Item.emug9cXuwndDrWzu";
-  if (value === true) {set.add(knockback);}
-  else {set.delete(knockback);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addMelee", {
-  name: "Melee Free Strike",
-  hint: "Melee Free Strike is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const melee = "Compendium.draw-steel.abilities.Item.wU69Y06G9lYFrvp6";
-  if (value === true) {set.add(melee);}
-  else {set.delete(melee);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addRanged", {
-  name: "Ranged Free Strike",
-  hint: "Ranged Free Strike is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const ranged = "Compendium.draw-steel.abilities.Item.eqUobBcm81mqZVgJ";
-  if (value === true) {set.add(ranged);}
-  else {set.delete(ranged);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addRide", {
-  name: "Ride",
-  hint: "Ride is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const ride = "Compendium.draw-steel.abilities.Item.QXOkflcYF6DITJE3";
-  if (value === true) {set.add(ride);}
-  else {set.delete(ride);};
-  }
- });
-
-game.settings.register("modify-draw-steel-default-abilities", "addStandUp", {
-  name: "Stand Up",
-  hint: "Stand Up is added to a new hero by default.",
-  scope: "world",
-  config: true,
-  type: Boolean,
-  default: true,
-  onChange: value => {
-  const set = CONFIG.DRAW_STEEL.hero.defaultItems;
-  const standUp = "Compendium.draw-steel.abilities.Item.XeUU0Blvi0fy0b2G";
-  if (value === true) {set.add(standUp);}
-  else {set.delete(standUp);};
-  }
- });
-
+  game.settings.register("modify-draw-steel-default-abilities", settingID, {
+    name: basicAbility.name,
+    hint: basicAbility.name + " is added to a new hero by default.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    onChange: value => {
+    const set = CONFIG.DRAW_STEEL.hero.defaultItems;
+    if (value === true) {set.add(basicAbility.uuid);}
+    else {set.delete(basicAbility.uuid);};
+    }
+  });
+};
 });
